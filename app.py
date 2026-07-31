29a5bef5eef8f67de1a7afa369214db7319c9f0a
shriyasadana <shriyasadana07@gmail.com>
2026-07-31 15:44:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,12 +119,12 @@ def prompt_generator(model,query):
     
     #---------------------------WITH TABS----------------------------
     with tab1:
-    st.header("GENERATE IMAGE GIVE PROMPT")
-    if st.button("Click to generate: ", key="generate_img_button"):
-      with st.spinner("Running Agent.."):
-        data = f"https://image,pollinations.ai/{user_query}"
-        time.sleep(3)
-        st.image(data)
+      st.header("GENERATE IMAGE GIVE PROMPT")
+      if st.button("Click to generate: ", key="generate_img_button"):
+        with st.spinner("Running Agent.."):
+          data = f"https://image,pollinations.ai/{user_query}"
+          time.sleep(3)
+          st.image(data)
       
     with tab2:
       st.header("CHECK LATEST NEWS")
